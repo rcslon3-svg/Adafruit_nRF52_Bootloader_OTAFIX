ffa22a8bdd7d108fbd02397d90c2defe6c7664a8
rcslon3-svg <rcslon3@gmail.com>
2026-04-27 16:46:21 +0700

    Update DEVICE_NAME to 'EasyRide22 DFU'

--- src/boards/nice_nano/board.h
+++ src/boards/nice_nano/board.h
@@ -50,7 +50,7 @@
 #define BLEDIS_MANUFACTURER "SmartMoto.Asia"
 #define BLEDIS_MODEL        "EasyRide"
 
-#define DEVICE_NAME          "EasyRide DFU"
+#define DEVICE_NAME          "EasyRide22 DFU"
 
 //--------------------------------------------------------------------+
 // USB
